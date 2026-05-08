c9eaf5da9d3e5ca68cfbf8a539876f6e9c6503df
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-05-07 21:01:02 -0300

    Project created

---
+++ src/main/java/com/educandoweb/aulamaxvenda/AulamaxvendaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.aulamaxvenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulamaxvendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulamaxvendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/aulamaxvenda/AulamaxvendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.aulamaxvenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulamaxvendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
