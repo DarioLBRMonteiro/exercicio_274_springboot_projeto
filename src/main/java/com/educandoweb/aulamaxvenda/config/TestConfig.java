65371db24da05c3874fa369404ca564800442d82
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-05-09 23:22:12 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/aulamaxvenda/config/TestConfig.java
+++ src/main/java/com/educandoweb/aulamaxvenda/config/TestConfig.java
@@ -1,6 +1,5 @@
 package com.educandoweb.aulamaxvenda.config;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/educandoweb/aulamaxvenda/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/aulamaxvenda/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.educandoweb.aulamaxvenda.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.aulamaxvenda.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
