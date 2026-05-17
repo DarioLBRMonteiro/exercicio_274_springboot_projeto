7190142e57d3c7cd07f4cd1be3011bda035ecf40
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-05-17 17:53:40 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/aulamaxvenda/services/UserService.java
+++ src/main/java/com/educandoweb/aulamaxvenda/services/UserService.java
@@ -12,30 +12,32 @@ import com.educandoweb.aulamaxvenda.repositories.UserRepository;
 import com.educandoweb.aulamaxvenda.services.exceptions.DatabaseException;
 import com.educandoweb.aulamaxvenda.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			if (!repository.existsById(id))
 				throw new ResourceNotFoundException(id);
-			
+
 			repository.deleteById(id);
 		} catch (ResourceNotFoundException e) {
 			throw new ResourceNotFoundException(id);
@@ -43,13 +45,18 @@ public class UserService {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-	
+
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
